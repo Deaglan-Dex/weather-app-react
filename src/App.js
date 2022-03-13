91031609cfa23ca50df0d30eb7229ba53a19f988
h-hjaffery <hasnain.jafferyy@gmail.com>
2022-03-13 11:51:49 +0000

    tile made

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Navbar from './Navbar'
+import Tiles from './Tiles'
 import "./App.css"
 
 export default function App() {
   return (
     <div className='page'>
-      
+      <Tiles />
       <Navbar />
     </div>
   )
