1c614cb7de4fd89991bd51269c97dbec4b5c1b42
Your Name <you@example.com>
2022-03-22 21:07:29 +0000

    ahhh

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Todate from './Todate'
 import Currentemp from './Currentemp'
 import Circles from './Circles'
@@ -15,6 +15,8 @@ import TempInfo from "./TempInfo";
 import SunRiseSet from "./SunRiseSet";
 import Status from "./Status";
 import "./App.css"
+import cloudsbackg from "./cloudy2.jpg";
+import clearbackg from "./sunny.jpg";
 import {
   BrowserRouter as Router,
   Switch,
@@ -22,35 +24,95 @@ import {
   Link
 } from "react-router-dom";
 
-export default function App() {
+function hello(ahh) {
+  if (ahh === "Clouds") {
+    return "page-clouds";
+  }
+  else if (ahh === "Clear"){
+    return "page-clear";
+  }
+}
+
+class App extends Component {
+  state = {
+    loading: true,
+    weather: null
+  };
+
+  async componentDidMount() {
+    window.locale = 0;
+
+    if ("geolocation" in navigator) {
+      console.log("Available");
+      navigator.geolocation.getCurrentPosition(
+        function(position) {
+          // console.log("Latitude is :", position.coords.latitude);
+          // console.log("Longitude is :", position.coords.longitude);
+          localStorage.setItem('lat', position.coords.latitude);
+          localStorage.setItem('lon', position.coords.longitude);
+          window.locale = 1;
+        },
+        function(error) {
+          console.error("Error Code = " + error.code + " - " + error.message);
+        }
+      );
+    } else {
+      console.log("Not Available");
+    }
+    var lat = localStorage.getItem('lat');
+    var lon = localStorage.getItem('lon');
+    // console.log(lat)
+    // console.log(lon)
+    const url = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=0002ba6db11e43059c746878dacce316&units=metric";
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({ weather: data, loading: false });
+    console.log("ahhh" + url);
+    console.log(this.state.weather.weather[0].main);
+  }
+  render() {
+    if (window.locale == 0) {
+      return <div>Please enable location services and make sure browser has access to it</div>;
+    }
+
+    if (this.state.loading) {
+      return <div>loading...</div>;
+    }
+
+    if (!this.state.weather) {
+      return <div>didn't recieve weather information</div>;
+    }
+
+    var abcd = this.state.weather.weather[0].main;
     return (
       <Router>
-        <div className='page'>
-          <Switch>
-            <Route exact path='/'>
-              <Link to="/info?cnt=1">
-                <Todate />
-                <Currentemp />
-                <Circles />
-              </Link>
-              <Tiles />
-              <Location />
-            </Route>
-            <Route exact path='/sixteen'>
-              <Header />
-              <Days />
-            </Route>
-            <Route exact path='/info'>
-              <Datetimetemp />
-              <HighLowTemps />
-              <Status />
-              <SunRiseSet />
-              <TempInfo />
-              <InfoTable />
-            </Route>
-          </Switch>
-          <Navbar />
-        </div>
-      </Router>
-  )
+          <div className={hello(abcd)}>
+            <Switch>
+              <Route exact path='/'>
+                <Link to="/info?cnt=1">
+                  <Todate />
+                  <Currentemp />
+                  <Circles />
+                </Link>
+                <Tiles />
+                <Location />
+              </Route>
+              <Route exact path='/sixteen'>
+                <Header />
+                <Days />
+              </Route>
+              <Route exact path='/info'>
+                <Datetimetemp />
+                <HighLowTemps />
+                <Status />
+                <SunRiseSet />
+                <TempInfo />
+                <InfoTable />
+              </Route>
+            </Switch>
+            <Navbar />
+          </div>
+        </Router>
+    )}
 }
+export default App;
