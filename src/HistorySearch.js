dcb18c46b794839d0982332ace195df5da188dbe
Your Name <you@example.com>
2022-03-23 14:39:18 +0000

    commit

--- src/HistorySearch.js
+++ src/HistorySearch.js
@@ -2,19 +2,30 @@ import React from 'react';
 import "./App.css";
 import { useState } from 'react';
 import {
-    Redirect
+    Link,
+    Redirect,
+    useHistory
   } from "react-router-dom";
 
 export default function HistorySearch() {
     const [day, setDay] = useState("");
     const [month, setMonth] = useState("");
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        return <Redirect to = "/history" />
-        // alert(`The day you entered was: ${day} and the month you entered was ${month}`);
-
+    const history = useHistory();
+  
+    const handleSubmit = () => {
+      history.push({
+        pathname: '/history',
+        search: `?month=${month}&day=${day}`
+      })
     }
+
+    // const handleSubmit = (event) => {
+    //     event.preventDefault();
+    //     var redirect = "/history?month=" + month + "&day=" + day;
+    //     <Redirect to={redirect} />
+    // }
+    var abcd = "/history" + day;
   return (
     <form>
         <fieldset className = 'searchForm'>
